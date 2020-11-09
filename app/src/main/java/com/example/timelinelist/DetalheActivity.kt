5e8ccf3977971b5acdd6811d5ee312eb87f4269d
manasomali <matheus.marques_96@hotmail.com>
2020-11-09 11:36:37 -0300

    first commit

---
+++ app/src/main/java/com/example/timelinelist/DetalheActivity.kt
@@ -0,0 +1,11 @@
+package com.example.timelinelist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalheActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe)
+    }
+}
