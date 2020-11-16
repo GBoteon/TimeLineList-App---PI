7cb258391989d3ffdbbe7ba36fb7fce0dad77ba5
manasomali <matheus.marques_96@hotmail.com>
2020-11-16 12:21:44 -0300

    esquema de temas feito

--- app/src/main/java/com/example/timelinelist/ListaActivity.kt
+++ app/src/main/java/com/example/timelinelist/ListaActivity.kt
@@ -11,12 +11,6 @@ class ListaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        val mode = resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)
-        when (mode) {
-            Configuration.UI_MODE_NIGHT_YES -> {image_logo.setImageResource(R.drawable.nome_escuro)}
-            Configuration.UI_MODE_NIGHT_NO -> {image_logo.setImageResource(R.drawable.nome_claro)}
-            Configuration.UI_MODE_NIGHT_UNDEFINED -> {image_logo.setImageResource(R.drawable.nome_claro)}
-        }
+
     }
 }
