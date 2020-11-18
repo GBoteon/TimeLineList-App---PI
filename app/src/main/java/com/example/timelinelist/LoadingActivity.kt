6952922c2f9c9c3b35a950743ed30e9560c5bba1
rodolfo <rodolfo.ruyz@gmail.com>
2020-11-18 15:34:46 -0300

    Coroutine Splash

--- app/src/main/java/com/example/timelinelist/LoadingActivity.kt
+++ app/src/main/java/com/example/timelinelist/LoadingActivity.kt
@@ -1,11 +1,28 @@
 package com.example.timelinelist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.*
 
 class LoadingActivity : AppCompatActivity() {
+
+    val scope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
+
+        scope.launch {
+            delay(3000)
+
+            var intent = Intent(this@LoadingActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    override fun onPause() {
+        scope.cancel()
+        super.onPause()
     }
 }
