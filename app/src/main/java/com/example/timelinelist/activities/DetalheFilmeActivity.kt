954347a4bb979bcb316cbbda89d3d7fab15ea374
manasomali <matheus.marques_96@hotmail.com>
2020-11-25 18:15:35 -0300

    ajustes na interface

--- app/src/main/java/com/example/timelinelist/activities/DetalheFilmeActivity.kt
+++ app/src/main/java/com/example/timelinelist/activities/DetalheFilmeActivity.kt
@@ -1,12 +1,22 @@
 package com.example.timelinelist.activities
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.example.timelinelist.R
 import kotlinx.android.synthetic.main.activity_detalhefilme.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class DetalheFilmeActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhefilme)
@@ -20,5 +30,27 @@ class DetalheFilmeActivity : AppCompatActivity() {
             val shareIntent = Intent.createChooser(sendIntent, null)
             startActivity(shareIntent)
         }
+        val myCalendar: Calendar = Calendar.getInstance()
+
+        val date =
+            OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                myCalendar.set(Calendar.YEAR, year)
+                myCalendar.set(Calendar.MONTH, monthOfYear)
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateLabel(myCalendar)
+            }
+
+        edittext_data.setOnClickListener {
+            DatePickerDialog(
+                this@DetalheFilmeActivity, R.style.DialogTheme, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+    }
+    private fun updateLabel(myCalendar: Calendar) {
+        val myFormat = "dd/MM/yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        edittext_data.setText(sdf.format(myCalendar.getTime()))
     }
 }
