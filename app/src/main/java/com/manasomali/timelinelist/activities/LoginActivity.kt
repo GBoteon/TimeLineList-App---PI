7790ae5ea236e85c6a97a8bc997fb71de3c2f194
manasomali <matheus.marques_96@hotmail.com>
2021-01-13 09:25:32 -0300

    login firebase email e senha pronto

--- app/src/main/java/com/manasomali/timelinelist/activities/LoginActivity.kt
+++ app/src/main/java/com/manasomali/timelinelist/activities/LoginActivity.kt
@@ -1,25 +1,51 @@
 package com.manasomali.timelinelist.activities
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.manasomali.timelinelist.Constants
 import com.manasomali.timelinelist.R
+import kotlinx.android.synthetic.main.activity_cadastro.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    val sharedPrefs by lazy {  getSharedPreferences(Constants.PREFS_NAME, Context.MODE_PRIVATE) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         requestedOrientation = SCREEN_ORIENTATION_PORTRAIT
 
         button_login.setOnClickListener {
+            singinUsuarioFirebase(edittext_login_email.text.toString(), edittext_login_senha.text.toString())
             val intent = Intent(this, ListaActivity::class.java)
             startActivity(intent)
         }
         button_cadastro.setOnClickListener {
             val intent = Intent(this, CadastroActivity::class.java)
             startActivity(intent)
+        }
+    }
+    fun singinUsuarioFirebase(email: String, senha: String) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener {
+                task ->
+            if(task.isSuccessful) {
+                val emailUser = task.result?.user!!.email
+                sharedPrefs.edit().putString(Constants.KEY_IDUSER, task.result?.user!!.uid).apply()
+                sharedPrefs.edit().putString(Constants.KEY_NOME,
+                    edittext_cadastro_nome.text.toString()).apply()
+                sharedPrefs.edit().putString(Constants.KEY_SOBRENOME,
+                    edittext_cadastro_sobrenome.text.toString()).apply()
+                sharedPrefs.edit().putString(Constants.KEY_EMAIL, emailUser).apply()
+                startActivity(Intent(this, ListaActivity::class.java))
+            } else {
+                Toast.makeText(this, task.exception?.message.toString(), Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
